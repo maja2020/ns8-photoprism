95371b417a105152e757ff380c543c153e6c2c99
maja2020 <32190313+maja2020@users.noreply.github.com>
2026-06-06 14:51:14 +0000

    changed photoprism docker build to 20260601

--- build-images.sh
+++ build-images.sh
@@ -40,7 +40,9 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
     --label="org.nethserver.min-core=3.12.4-0" \
-    --label="org.nethserver.images=docker.io/mariadb:11 docker.io/photoprism/photoprism:260305" \
+    --label="org.nethserver.images=docker.io/mariadb:11 docker.io/photoprism/photoprism:260601
+
+" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
